6dfcc234007c0682ad08cd91ab4dd576ce4ba516
Pratyush shankar <pratyushshanker07@gmail.com>
2025-10-01 03:29:35 +0530

    update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1M75KgKu8gbuBQ26I5rHjQC7EsaNZJ1M8/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Vj-C6-jY77r1hyfZjvrEjMPernozQQO2/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
