07b548fe4faf55dae875c38e4d5e029be30f215f
Pratyush shankar <pratyushshanker07@gmail.com>
2025-10-01 02:50:35 +0530

    Deployment setup: fixed bugs and updated build

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_eut9xuj",  // Replace with your EmailJS Service ID
+        "service_otzk99h",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "Dox8Hshlyo2yr931s"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   // Smooth scroll function
   const handleScroll = (sectionId) => {
     const section = document.getElementById(sectionId);
+    console.log("Scrolling to:", section);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
@@ -14,7 +15,7 @@ const Footer = () => {
     <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
       <div className="container mx-auto text-center">
         {/* Name / Logo */}
-        <h2 className="text-xl font-semibold text-purple-500">Tarun Kaushik</h2>
+        <h2 className="text-xl font-semibold text-purple-500">Pratyush Shankar</h2>
 
         {/* Navigation Links - Responsive */}
         <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
@@ -22,7 +23,7 @@ const Footer = () => {
             { name: "About", id: "about" },
             { name: "Skills", id: "skills" },
             { name: "Experience", id: "experience" },
-            { name: "Projects", id: "projects" },
+            { name: "Projects", id: "Projects" },
             { name: "Education", id: "education" },
           ].map((item, index) => (
             <button
@@ -38,11 +39,11 @@ const Footer = () => {
         {/* Social Media Icons - Responsive */}
         <div className="flex flex-wrap justify-center space-x-4 mt-6">
           {[
-            { icon: <FaFacebook />, link: "https://www.facebook.com/tarun.kaushik.3511041/" },
-            { icon: <FaTwitter />, link: "https://twitter.com/CodingMaster6?s=09" },
-            { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/tarun-kaushik-553b441a4" },
-            { icon: <FaInstagram />, link: "https://www.instagram.com/coding_.master/" },
-            { icon: <FaYoutube />, link: "https://www.youtube.com/codingmasteryt" },
+            { icon: <FaFacebook />, link: "#" },
+            { icon: <FaTwitter />, link: "#" },
+            { icon: <FaLinkedin />, link: "#" },
+            { icon: <FaInstagram />, link: "#" },
+            { icon: <FaYoutube />, link: "#" },
             
           ].map((item, index) => (
             <a
@@ -59,7 +60,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Tarun Kaushik. All rights reserved.
+          © 2025 Pratyush Shankar. All rights reserved.
         </p>
       </div>
     </footer>
